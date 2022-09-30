ae20304077dd6c2ac12d09002cda1b943c654c58
Evan Wells <etwe226@uky.edu>
2022-09-30 14:39:41 -0400

    Changed to read serial by line

--- main.cpp
+++ main.cpp
@@ -1,50 +1,30 @@
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <errno.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 #include "config.h"
 
-int main(void)
+int main()
 {
   int fd;
-  int count;
-  unsigned int nextTime;
-
   if ((fd = serialOpen(SERIAL_FEATHER, 115200)) < 0)
   {
     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
     return 1;
   }
-
+  printf("Connected to %s!\n", SERIAL_FEATHER);
   if (wiringPiSetup() == -1)
   {
     fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
     return 1;
   }
-
-  nextTime = millis() + 300;
-
-  for (count = 0; count < 256;)
+  printf("wiringPi set up!\n");
+  printf("Listening for serial!\n");
+  for (;;)
   {
-    if (millis() > nextTime)
-    {
-      printf("\nOut: %3d: ", count);
-      fflush(stdout);
-      serialPutchar(fd, count); // Sends over TX
-      nextTime += 300;
-      ++count;
-    }
-
-    delay(3);
-
-    while (serialDataAvail(fd))
-    {
-      printf(" -> %3d", serialGetchar(fd)); // Receives from RX
-      fflush(stdout);
-    }
+    putchar (serialGetchar (fd)) ;
+    fflush (stdout) ;
   }
-
-  printf("\n");
   return 0;
 }
