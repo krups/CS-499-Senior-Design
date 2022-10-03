7f39de38ab29454c0c8ce9701ba4621409ddc2f1
Christopher Cox <chriscox228@gmail.com>
2022-10-03 18:44:46 -0400

    Store received data in vectors instead of printing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <wiringSerial.h>
 #include "config.h"
 
+#include <iostream> // used for printing vectors in tests
+#include <vector> // temporarily store data
+#include <string> 
+
 int main()
 {
   int fd;
@@ -21,10 +25,60 @@ int main()
   }
   printf("wiringPi set up!\n");
   printf("Listening for serial!\n");
-  for (;;)
+
+  // Variables for reading in data
+  char letter;
+  std::string section = "";
+  std::string line = "";
+  std::vector<int> split_line;
+  std::vector<std::vector<int>> data;
+
+  for (int x = 0;;)
   {
-    putchar (serialGetchar (fd)) ;
-    fflush (stdout) ;
+    // Gets the first int, converts to char and adds to string
+    letter = (char)serialGetchar(fd);
+    line += letter;
+
+    // End of line
+    if (letter == '\n')
+    {
+      // Loop through every letter
+      for (char letter: line)
+      {
+        // End of section
+        if (letter == ',')
+        {
+          split_line.push_back(std::stoi(section));
+          section = "";
+        }
+        // Section continues
+        else
+        {
+          section += letter;
+        }
+      }
+
+      // Add data to vectors
+      split_line.push_back(std::stoi(section));
+      data.push_back(split_line);
+
+      // Resets variables
+      line = "";
+      section = "";
+      split_line.clear();
+
+      // Prints all data within vector that was just saved, if PRINT_DATA is set
+      if (PRINT_DATA)
+      {
+        for (int j=0; j < (int) data.at(x).size(); j++)
+        {
+          std::cout << data.at(x).at(j) << ' ';
+        }
+        std::cout << '\n';
+        x++;
+      }
+    }
   }
+  
   return 0;
 }
