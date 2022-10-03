cbe5f5571b29af8c87169439604d0f1d1dac32c9
Christopher Cox <chriscox228@gmail.com>
2022-10-03 18:45:20 -0400

    Added configuration for printing data or not.

--- config.h
+++ config.h
@@ -1,8 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Serial port which interfaces with Feather M4 Express 
+// Serial port which interfaces with Feather M4 Express
 #define SERIAL_FEATHER "/dev/ttyS1"
+
+// Prints data being received if set to 1, disables when set to 0
+#define PRINT_DATA 1
 
 #define DEBUG // comment/uncomment to enable/disable debugging logs
 #ifdef DEBUG
