3dc89d01ff4d82f9dae5588a519a82d870ce7cdd
Christopher Cox <chriscox228@gmail.com>
2022-10-05 01:13:01 -0400

    Add new vars. Move printing to debug section.

--- config.h
+++ config.h
@@ -4,12 +4,18 @@
 // Serial port which interfaces with Feather M4 Express
 #define SERIAL_FEATHER "/dev/ttyS1"
 
-// Prints data being received if set to 1, disables when set to 0
-#define PRINT_DATA 1
+// Which hex should be seen as a command
+#define COMMAND 0x301
+
+// Max characters for line buffer
+#define MAX_CHARS 256
 
 #define DEBUG // comment/uncomment to enable/disable debugging logs
 #ifdef DEBUG
 // add toggles for feature debugging
+
+// Prints data being received
+#define PRINT_DATA
 
 #endif
 
