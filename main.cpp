67e45bc2d9a967aabc7ae15866630fe6bdf20f03
Joshua Morford <joshua55550000@hotmail.com>
2022-10-31 13:27:04 -0400

    Move sensor list initialization again

--- main.cpp
+++ main.cpp
@@ -18,14 +18,6 @@ char packetBuffer[PACKET_SIZE];
 sem_t packetSem;
 sem_t sensor1Sem;
 
-SensorList sensors;
-
-// Active Sensors
-// Entries should be formatted: FIGURE THIS OUT
-sensors.addSensor(THERMOCOUPLE, 1);
-sensors.addSensor(SPECTROMETER, 1);
-sensors.addSensor(ACCELEROMETER, 1);
-
 void saveData(Data data)
 {
 #ifdef PRINT_DATA
@@ -197,6 +189,13 @@ void *IOThread(void *arguments)
 
 int main()
 {
+    SensorList sensors;
+
+    // Active Sensors
+    // Entries should be formatted: FIGURE THIS OUT
+    sensors.addSensor(THERMOCOUPLE, 1);
+    sensors.addSensor(SPECTROMETER, 1);
+    sensors.addSensor(ACCELEROMETER, 1);
 
     // Start semaphores
     if (sem_init(&packetSem, 0, 1) != 0)
