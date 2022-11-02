7e283b20f1092811c3fb7ef837ac377f48a9dd06
Joshua Morford <joshua55550000@hotmail.com>
2022-11-02 14:50:05 -0400

    Add include guards to DataPoint.h

--- DataPoint.h
+++ DataPoint.h
@@ -1,6 +1,11 @@
+#ifndef DATA_POINT_H
+#define DATA_POINT_H
+
 struct DataPoint {
   uint16_t fileIndex;
   sensor_timestamp_t timestamp;
   sensor_id_t sensor_id;
   uint8_t numIncludes;
 };
+
+#endif
