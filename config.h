32c70148fc74bdf49d39f1950bf77d542e447b24
Joshua Morford <joshua55550000@hotmail.com>
2022-11-05 14:57:33 -0400

    Add more defines to config related to sensors

--- config.h
+++ config.h
@@ -6,6 +6,12 @@
 
 // Maximum packet size in bytes
 #define PACKET_SIZE 340
+#define PACKET_SIZE_BITS PACKET_SIZE * 8
+
+// Sensor metadata size in bytes
+#define SENSOR_METADATA_BITS 16
+#define SENSOR_ID_BITS 3
+#define SENSOR_TIMESTAMP_BITS SENSOR_METADATA_BITS - SENSOR_ID_BITS
 
 // Serial port which interfaces with Feather M4 Express
 #define SERIAL_FEATHER "/dev/ttyS1"
@@ -20,8 +26,16 @@ const std::string SENSOR_DATA_PATH = "./data/"; // This directory must exist bef
 #define SPECTROMETER 2
 #define ACCELEROMETER 3
 
+// Validity parameters
+// Check for erroneous data 
+#define TC_LOW -1000
+#define TC_MAX 10000
+
+#define ACC_LOW -100
+#define ACC_HIGH 100
+
 // Data Selection Config
-#define BYTE_TARGET 500
+#define BYTE_TARGET PACKET_SIZE
 #define NEW_DATA_RATIO 0.8
 #define POINT_INCLUDE_LIMIT 1
 
@@ -31,13 +45,5 @@ const std::string SENSOR_DATA_PATH = "./data/"; // This directory must exist bef
 #define PACKET_P
 #define VALIDITY_P
 #endif
-
-// Validity parameters
-// Check for erroneous data 
-#define TC_LOW -1000
-#define TC_MAX 10000
-
-#define ACC_LOW -100
-#define ACC_HIGH 100
 
 #endif
