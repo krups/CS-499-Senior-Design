a9aea4f76e534432f6760953cb01fb0b238eaf47
Joshua Morford <joshua55550000@hotmail.com>
2022-11-09 11:10:53 -0500

    Fix sensor config values not matching

--- main.cpp
+++ main.cpp
@@ -221,10 +221,10 @@ void *IOThread(void *arguments)
 int main()
 {
     // Active Sensors
-    // Entries should be formatted: sensor_id, sensor_priority, num_samples, num_bits_per_sample
-    sensors.addSensor(THERMOCOUPLE_ID, THERMOCOUPLE_PRIORITY, THERMOCOUPLE_NUM_SAMPLES, THERMOCOUPLE_NUM_BITS_PER_SAMPLE);
-    sensors.addSensor(SPECTROMETER_ID, SPECTROMETER_PRIORITY, SPECTROMETER_NUM_SAMPLES, SPECTROMETER_NUM_BITS_PER_SAMPLE);
-    sensors.addSensor(ACCELEROMETER_ID, ACCELEROMETER_PRIORITY, ACCELEROMETER_NUM_SAMPLES, ACCELEROMETER_NUM_BITS_PER_SAMPLE);
+    // Entries should be formatted: sensor_id, sensor_priority, num_samples_per_data_point, num_bits_per_sample
+    sensors.addSensor(THERMOCOUPLE_ID, THERMOCOUPLE_PRIORITY, THERMOCOUPLE_NUM_SAMPLES_PER_DATA_POINT, THERMOCOUPLE_NUM_BITS_PER_SAMPLE);
+    sensors.addSensor(SPECTROMETER_ID, SPECTROMETER_PRIORITY, SPECTROMETER_NUM_SAMPLES_PER_DATA_POINT, SPECTROMETER_NUM_BITS_PER_SAMPLE);
+    sensors.addSensor(ACCELEROMETER_ID, ACCELEROMETER_PRIORITY, ACCELEROMETER_NUM_SAMPLES_PER_DATA_POINT, ACCELEROMETER_NUM_BITS_PER_SAMPLE);
 
     // Start semaphores
     if (sem_init(&packetSem, 0, 1) != 0)
