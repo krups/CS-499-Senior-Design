d8e746b28e1cdbc82e64fa6ea34dac51feb27c56
Evan Wells <etwe226@uky.edu>
2022-12-05 13:12:39 -0500

    removed unnecessary trim functions

--- data.cpp
+++ data.cpp
@@ -1,24 +1,5 @@
 #include "data.h"
 
-const std::string WHITESPACE = " \n\r\t\f\v";
-
-std::string ltrim(const std::string &s)
-{
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
-}
-
-std::string rtrim(const std::string &s)
-{
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
-
-std::string trim(const std::string &s)
-{
-    return rtrim(ltrim(s));
-}
-
 /**
  * @brief Construct a new Data:: Data object
  *        Based on format of incoming raw data:
