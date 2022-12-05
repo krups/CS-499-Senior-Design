81bc5b69515e38c8e777967386ab3ecaa7aa06eb
Evan Wells <etwe226@uky.edu>
2022-12-05 13:14:09 -0500

    Updated ID's in config for sensor testbed

--- config.h
+++ config.h
@@ -37,24 +37,8 @@ const std::string PACKET_DATA_PATH = "./packets/"; // This directory must exist 
 //
 //////////////////////////////
 
-// TC Sensors
-#define TC_ID 1
-#define TC_PRIORITY 1
-#define TC_NUM_SAMPLES_PER_DATA_POINT 4
-#define TC_NUM_BITS_PER_SAMPLE 11
-#define TC_OFFSET (-1 * TC_LOW)
-#define TC_MULT 1
-
-// IMU Sensor
-#define IMU_ID 2
-#define IMU_PRIORITY 1
-#define IMU_NUM_SAMPLES_PER_DATA_POINT 6
-#define IMU_NUM_BITS_PER_SAMPLE 16
-#define IMU_OFFSET 0
-#define IMU_MULT 1
-
 // GPS 
-#define GPS_ID 3
+#define GPS_ID 1
 #define GPS_PRIORITY 1
 #define GPS_NUM_SAMPLES_PER_DATA_POINT 5
 #define GPS_NUM_BITS_PER_SAMPLE 16
@@ -62,7 +46,7 @@ const std::string PACKET_DATA_PATH = "./packets/"; // This directory must exist 
 #define GPS_MULT 1
 
 // RMC 
-#define RMC_ID 4
+#define RMC_ID 2
 #define RMC_PRIORITY 1
 #define RMC_NUM_SAMPLES_PER_DATA_POINT 5
 #define RMC_NUM_BITS_PER_SAMPLE 16
@@ -70,12 +54,28 @@ const std::string PACKET_DATA_PATH = "./packets/"; // This directory must exist 
 #define RMC_MULT 1
 
 // ACC sensor
-#define ACC_ID 5
+#define ACC_ID 3
 #define ACC_PRIORITY 1
 #define ACC_NUM_SAMPLES_PER_DATA_POINT 3
 #define ACC_NUM_BITS_PER_SAMPLE 8
 #define ACC_OFFSET 0
 #define ACC_MULT 1
+
+// IMU Sensor
+#define IMU_ID 4
+#define IMU_PRIORITY 1
+#define IMU_NUM_SAMPLES_PER_DATA_POINT 6
+#define IMU_NUM_BITS_PER_SAMPLE 16
+#define IMU_OFFSET 0
+#define IMU_MULT 1
+
+// TC Sensors
+#define TC_ID 5
+#define TC_PRIORITY 1
+#define TC_NUM_SAMPLES_PER_DATA_POINT 4
+#define TC_NUM_BITS_PER_SAMPLE 11
+#define TC_OFFSET (-1 * TC_LOW)
+#define TC_MULT 1
 
 // PRES sensors
 #define PRES_ID 6
@@ -86,7 +86,7 @@ const std::string PACKET_DATA_PATH = "./packets/"; // This directory must exist 
 #define PRES_MULT 10
 
 // Spectrometer
-#define SPEC_ID 10
+#define SPEC_ID 7
 #define SPEC_PRIORITY 1
 #define SPEC_NUM_SAMPLES_PER_DATA_POINT 5
 #define SPEC_NUM_BITS_PER_SAMPLE 4
@@ -116,8 +116,8 @@ const std::string PACKET_DATA_PATH = "./packets/"; // This directory must exist 
 #define NEW_DATA_RATIO 0.8
 #define POINT_INCLUDE_LIMIT 1
 
-#define NEW_DATA_GRADIENT_SELECT true
-#define OLD_DATA_GRADIENT_SELECT true
+#define NEW_DATA_GRADIENT_SELECT false
+#define OLD_DATA_GRADIENT_SELECT false
 
 //////////////////////////////
 //
