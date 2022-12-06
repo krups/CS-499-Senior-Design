455fc5f7a316939d4e8239748c2067058f0fd783
Joshua Morford <joshua55550000@hotmail.com>
2022-12-05 21:30:53 -0500

    Add comments to decode program

--- decode.cpp
+++ decode.cpp
@@ -12,6 +12,7 @@ SensorMap sensors;
 
 int main()
 {
+  // Setup sensor configuration
   sensors.addSensor(TC_ID, TC_PRIORITY, TC_NUM_SAMPLES_PER_DATA_POINT, TC_NUM_BITS_PER_SAMPLE, TC_OFFSET, TC_MULT);
   sensors.addSensor(IMU_ID, IMU_PRIORITY, IMU_NUM_SAMPLES_PER_DATA_POINT, IMU_NUM_BITS_PER_SAMPLE, IMU_OFFSET, IMU_MULT);
   sensors.addSensor(GPS_ID, GPS_PRIORITY, GPS_NUM_SAMPLES_PER_DATA_POINT, GPS_NUM_BITS_PER_SAMPLE, GPS_OFFSET, GPS_MULT);
@@ -25,80 +26,106 @@ int main()
 
   std::string filename;
 
+  // Request packet file to decode
   std::cout << "Please enter the name of the file that you want to decode: ";
   std::cin >> filename;
 
   path += filename;
 
-  char data[PACKET_SIZE];
-  memset(data, '\0', PACKET_SIZE);
+  // Prepare a buffer to read in packet data
+  // Add an extra null byte to make sure you don't accidentally read past the end of the packet when processing the packet data
+  char data[PACKET_SIZE + 1];
+  memset(data, '\0', PACKET_SIZE + 1);
 
+  // Open the packet file and read the contents
   packetFile.open(path, std::ios_base::in);
   if (!packetFile.fail())
   {
     packetFile.read(data, PACKET_SIZE);
   }
+  // Output error message if open fails
   else
   {
     std::cout << "Error opening packet file" << std::endl;
     return 1;
   }
 
+  // Proceed until an error is detected
   bool moreData = true;
 
   unsigned int bitIndex = 0;
 
   while (moreData)
   {
+    // Attempt to extract a sensor ID from the packet data
     unsigned int sensorId = 0;
 
     copyBitsB2L((uint8_t *)data, bitIndex, (uint8_t *)&sensorId, (sizeof(unsigned int) * 8) - SENSOR_ID_BITS, sizeof(unsigned int), SENSOR_ID_BITS);
 
     bitIndex += SENSOR_ID_BITS;
+
+    // If the sensor ID is nonzero (it was modified, indicating that data exists and it's not the end of the packet)
     if (sensorId != 0)
     {
+      // If the sensor ID is a valid sensor ID
       if (sensors.sensorMap[sensorId] != nullptr)
       {
+        // Begin outputing info for this data point
         std::cout << sensorId;
 
+        // Extract the timestamp and display it
         unsigned int timestamp = 0;
         copyBitsB2L((uint8_t *)data, bitIndex, (uint8_t *)&timestamp, (sizeof(unsigned int) * 8) - SENSOR_TIMESTAMP_BITS, sizeof(unsigned int), SENSOR_TIMESTAMP_BITS);
         bitIndex += SENSOR_TIMESTAMP_BITS;
         std::cout << ", " << timestamp;
 
+        // For every sample that exists for the designated sensor
         for (unsigned i = 0; i < sensors.sensorMap[sensorId]->numSamplesPerDataPoint; i++)
         {
+          // Read the sample value
           int value = 0;
           copyBitsB2L((uint8_t *)data, bitIndex, (uint8_t *)&value, (sizeof(unsigned int) * 8) - sensors.sensorMap[sensorId]->numBitsPerSample, sizeof(unsigned int), sensors.sensorMap[sensorId]->numBitsPerSample);
           bitIndex += sensors.sensorMap[sensorId]->numBitsPerSample;
 
+          // If this sensor uses a multiplier for fixed precision floating point numbers
           if (sensors.sensorMap[sensorId]->multiplier != 1) {
+            // Convert the value to a floating point number and remove the multiplier
             double valuePrecise = (double) value / (double) sensors.sensorMap[sensorId]->multiplier;
 
+            // If this sensor uses an offset, remove the offset
             if (sensors.sensorMap[sensorId]->offset != 0) {
               valuePrecise -= (double) sensors.sensorMap[sensorId]->offset;
             }
 
+            // Output the extracte value
             std:: cout << ", " << valuePrecise;
+          // If this sensor doesn't use a multiplier
           } else {
+            // Still check if it uses an offset and remove it if necessary
             if (sensors.sensorMap[sensorId]->offset != 0) {
               value -= sensors.sensorMap[sensorId]->offset;
             }
 
+          // Display the extracted value
           std:: cout << ", " << value;
           }
         }
 
+        // Add a newline at the end of that data point
         std::cout << std::endl;
       }
+      // If the sensor ID is invalid
       else
       {
+        // Output an error and quit
         std::cout << "Unknown sensor type" << std::endl;
         moreData = false;
       }
     }
+    // If no data was read
     else
     {
+      // Indicate the end of the packet
       std::cout << "End of packet" << std::endl;
       moreData = false;
     }
